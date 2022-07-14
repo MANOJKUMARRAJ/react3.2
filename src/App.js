a0e52584f303cb9b22668af808eb72a1cd594162
MANOJKUMARRAJ <manoj.kumar@techmojo.in>
2022-07-14 12:33:42 +0530

    reactassignment3.2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import User from './component/User';
+import Users from './users.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <User/>
+      {
+      Users.map( user =>{
+        return(
+          <tr className="man" key={user.id}>
+            
+            <td>{user.id}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+            <td>{user.username}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+            <td>{user.email}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+            <td>{user.name.firstname}{user.name.lastname}</td>
+            <td>{user.phone}</td>
+          </tr>
+        )
+      })
+    }
     </div>
   );
 }
---
+++ src/component/User.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function user() {
+  return (
+    <div>
+        <table>
+            <tr>
+                <th>SerialNo&nbsp;&nbsp;&nbsp;&nbsp;</th>
+                <th>Username&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
+                <th>Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
+                <th>name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
+                <th>Phone Number</th>
+            </tr>
+        </table>
+    </div>
+  )
+}
+
+export default user
